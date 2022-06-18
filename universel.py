b82305f37a13ddef01f236c9f3542eeff5a50f64
Harsh vishwakarma <106618650+harshv958@users.noreply.github.com>
2022-06-18 11:15:08 -0700

    Laptop class added

--- universel.py
+++ universel.py
@@ -1,14 +1,21 @@
 class Computer:
-  	def __init__(self, processor, ram, storage, graphic_card):
-			self.processor = processor
-			self.ram = ram
-			self.storage = storage
-			self.graphic_card = graphic_card
+	def __init__(self, processor, ram, storage, graphic_card):
+		self.processor = processor
+		self.ram = ram
+		self.storage = storage
+		self.graphic_card = graphic_card
     
   	def configuration(self):
-			return {
-				'processor':self.processor,
-				'ram':self.ram,
-				'graphic_card': self.graphic_card,
-				'storage':self.storage,
-			}
+		return {
+			'processor':self.processor,
+			'ram':self.ram,
+			'graphic_card': self.graphic_card,
+			'storage':self.storage,
+		}
+	
+
+	class Laptop(Computer):
+		def __init__(self, processor, ram, stroage, graphic_card):
+			self.super(processor, ram, storage, graphic_card)
+			
+		
