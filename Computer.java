4a99175cf6903fb577923174fd9e769e7840f4ad
Harsh vishwakarma <106618650+harshv958@users.noreply.github.com>
2022-06-18 11:25:21 -0700

    computer class in java

---
+++ Computer.java
@@ -0,0 +1,16 @@
+public class Computer
+{
+	private String processor;
+	private String ram;
+	private String graphic_card;
+	private String storage;
+	
+	public Computer(String processor, String ram, String graphic_card, String storage)
+	{
+		this.processor = processor;
+		this.ram = ram;
+		this.storage = storage;
+		this.graphic_card = graphic_card;
+	}
+	
+}
