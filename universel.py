713631c23c254dffd5f194de841e66c652fbc13a
Harsh vishwakarma <106618650+harshv958@users.noreply.github.com>
2022-06-18 11:08:48 -0700

    computer class added

---
+++ universel.py
@@ -0,0 +1,14 @@
+class Computer:
+  	def __init__(self, processor, ram, storage, graphic_card):
+			self.processor = processor
+			self.ram = ram
+			self.storage = storage
+			self.graphic_card = graphic_card
+    
+  	def configuration(self):
+			return {
+				'processor':self.processor,
+				'ram':self.ram,
+				'graphic_card': self.graphic_card,
+				'storage':self.storage,
+			}
